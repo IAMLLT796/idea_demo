e39626c3ca380a4a41af6122c1cd6d9385434a00
IAMLLT796 <lt.liu796@gmail.com>
2024-04-17 12:50:10 +0800

    refactor code on reflect

--- src/main/java/com/llt/Calculator.java
+++ src/main/java/com/llt/Calculator.java
@@ -5,7 +5,7 @@ package com.llt;
  * @Date 2024-04-17-11:10
  **/
 public class Calculator {
-    public int compute(int num1, int num2, char symbol) {
+    public int compute(int num1, int num2, String symbol) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         Computable compute = ComputeFactory.getCompute(symbol);
         return compute.compute(num1, num2);
     }
