f6e653229c64b86c8cde73eb2cbefb0f3067a51f
IAMLLT796 <lt.liu796@gmail.com>
2024-04-17 11:29:07 +0800

    add calculation test

---
+++ src/main/java/com/llt/Calculator.java
@@ -0,0 +1,11 @@
+package com.llt;
+
+/**
+ * @Author LLT
+ * @Date 2024-04-17-11:10
+ **/
+public class Calculator {
+    public int compute(int num1, int nums2, char symbol) {
+        return -1;
+    }
+}
---
+++ src/test/java/com/llt/CalculatorTest.java
@@ -0,0 +1,35 @@
+package com.llt;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Author LLT
+ * @Date 2024-04-17-11:13
+ **/
+/*
+TDD:
+1. Write a fail test
+2.Make the test pass
+3.Refactor the code
+ */
+public class CalculatorTest {
+    @Test
+    public void shouldReturn2When1Plus1() {
+        Calculator calculator = new Calculator();
+        int actualResult = calculator.compute(1, 2, '+');
+        int expectResult = 2;
+        Assert.assertEquals(expectResult, actualResult);
+    }
+
+    @Test
+    public void shouldReturn0When1Sub1() {
+        Calculator calculator = new Calculator();
+        int actualResult = calculator.compute(1, 1, '+');
+        int expectResult = 0;
+        Assert.assertEquals(expectResult, actualResult);
+    }
+
+}
