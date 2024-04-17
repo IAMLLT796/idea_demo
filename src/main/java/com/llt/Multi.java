a42f1457674f31b95b74c7471aa977c6d84a1857
IAMLLT796 <lt.liu796@gmail.com>
2024-04-17 12:57:44 +0800

    increment Multi class

---
+++ src/main/java/com/llt/Multi.java
@@ -0,0 +1,12 @@
+package com.llt;
+
+/**
+ * @Author LLT
+ * @Date 2024-04-17-12:50
+ **/
+public class Multi implements Computable {
+    @Override
+    public int compute(int num1, int num2) {
+        return num1 * num2;
+    }
+}
